f95b2e4f20866b0608a5744fd9900a4bbd07211a
tunarmemmedov2007-ui <tunar.memmedov2007@gmail.com>
2026-09-25 00:47:34 +0300

    Update LW1_TunarMammadov.m

--- LW1/LW1_TunarMammadov.m
+++ LW1/LW1_TunarMammadov.m
@@ -15,7 +15,9 @@ xlabel('X-as')
 ylabel('F_1 [-o-] i F_2 [-x-]')
 
 %% Complementary Task 
-N = 5; 
+
+
+N = 4;
 
 v = (N+1) : 0.5 : (N+4);
 
@@ -24,8 +26,10 @@ A = [N   N+1 N+2;
      N+6 N+7 N+8];
 
 a_elem = A(3, 2);
+
 b_block = A(2:3, 1:2);
+
 c_corners = A([1, 3], [1, 3]);
 
-v_sub = v(1:3)'; 
-A_concat = [A, v_sub];
+v_sub = v(1:3)';     
+A_concat = [A, v_sub]; 
